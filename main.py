1ae1c4cbc5ce6ddc41e26ca1605f0b8a2bc8df5f
Sammie Nguyen <sammienguyen@Sammies-MBP.modem>
2022-11-29 00:22:31 +1100

    add print good night

--- main.py
+++ main.py
@@ -4,3 +4,14 @@ print("Hello World")
 
 # Steps to push
 # git add ./files
+# git commit -m "message"
+# git push origin main
+
+# Useful commands
+# ls: list all files, directories
+# mkdir: make a new directory
+# rm: remove files
+# rm -r: remove folders
+# rm -rf (Do not use)
+
+print("good night")
